2a1f7084e26944c6cd8f60838dd5d80e25c72d48
jelan mathew james <jelanmathewjames1234@gmail.com>
2021-12-18 21:49:11 +0530

    Custom User model is created login page created admin login page created

---
+++ Login/manager.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None,
+                    is_active=True, is_superuser=False,
+                    is_staff=False, **extra_fields):
+        if not email:
+            raise ValueError('Email is require')
+        user = self.model(
+            email=self.normalize_email(email),
+            is_active = is_active,
+            is_superuser = is_superuser,
+            is_staff = is_staff,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self,email,password,
+                        is_active=True, is_superuser=True,
+                        is_staff=True, **extra_fields):
+        if is_staff is not True:
+            raise ValueError("Super user must have is_staff True")
+        user = self.create_user(
+            email,
+            password,
+            is_staff=is_staff,
+            is_superuser = is_superuser,
+            is_active = is_active,
+            **extra_fields
+        )
+        user.save(using=self._db)
+        return user
---
+++ Login/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    mobile = models.CharField(max_length=30)
+    email_token = models.CharField(max_length=100,null=True,blank=True)
+    forgot_password = models.CharField(max_length=100,null=True,blank=True)
+    date_joined = models.DateTimeField(null=True,blank=True)
+    last_login = models.DateTimeField(null=True,blank=True)
+    last_logout = models.DateTimeField(null=True,blank=True)
+    object = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name','mobile']
