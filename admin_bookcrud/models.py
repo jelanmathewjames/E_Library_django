4c733894ff097ddf2746f74f107efb44e6501d74
jelan mathew james <jelanmathewjames1234@gmail.com>
2022-03-31 19:47:42 +0530

    changes in User model and Book model

--- admin_bookcrud/models.py
+++ admin_bookcrud/models.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     book_name = models.CharField(max_length=20)
     book_description = models.CharField(max_length=20)
     book_image = models.ImageField(upload_to='book_pics/')
+    book_price = models.FloatField(max_length=20)
+    book_inhand = models.BooleanField(default=False)
+    book_author = models.CharField(max_length=20)
