f030a066b0e4343ea405b19e2f73389425d96e74
jelan mathew james <jelanmathewjames1234@gmail.com>
2022-04-09 21:48:29 +0530

    changes in settings

--- E_Library/settings.py
+++ E_Library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n5h&x0@7y_&7gdcv18)^dk3diy!pq-ksqxuhm@=mmo_3kp^e=l
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
