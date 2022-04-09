3d1411baccdbe39a5a11300b03f5e7065af507d1
jelan mathew james <jelanmathewjames1234@gmail.com>
2022-04-09 21:42:20 +0530

    changes in settings.py

--- E_Library/settings.py
+++ E_Library/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
+
+SECRET_KEY = 'django-insecure-n5h&x0@7y_&7gdcv18)^dk3diy!pq-ksqxuhm@=mmo_3kp^e=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'E_Library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
+        'NAME' : 'library',
         'USER': 'postgres',
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST')
+        'PASSWORD' : 'jelan',
+        'HOST' : 'localhost'
     }
 }
 
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'elibrarycea@gmail.com'
+EMAIL_HOST_PASSWORD = '#m3zqrPna*ft0o'
