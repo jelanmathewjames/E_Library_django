9a11ce30ecceeb53dd1d4e25244728085602ab77
jelan mathew james <jelanmathewjames1234@gmail.com>
2022-04-10 19:26:20 +0530

    resolving bug due to not removing url for verify changing debug = false

--- Login/urls.py
+++ Login/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('login',views.login,name="login"),
     path('logout',views.logout,name="logout"),
-    path('verify/<email_token>',views.verify,name="verify"),
     path('',views.home,name="home")
 ]
